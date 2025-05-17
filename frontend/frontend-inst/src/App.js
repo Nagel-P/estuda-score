cb115e4848163b0efe8bbe4ea5b5baf2aacb70fa
Brian0505 <briancris2003@gmail.com>
2025-05-16 23:03:33 -0300

    Estilização Tela Inicial

---
+++ frontend/frontend-inst/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TelaInicial from './pages/TelaInicial/TelaInicial.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <TelaInicial />
+    </div>
+  );
+}
+
+export default App;
