95650983390dfa3d6f2c4bc2f717579c4288eed3
Brian0505 <briancris2003@gmail.com>
2025-05-19 20:27:59 -0300

    Tela Inclusao-Notas

--- frontend/frontend-inst/src/App.js
+++ frontend/frontend-inst/src/App.js
@@ -1,14 +1,19 @@
+// App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import TelaInicial from './pages/TelaInicial/TelaInicial';
 import PainelAluno from './pages/PainelAluno/PainelAluno';
+import TelaInclusaoNotas from './pages/TelaInclusaoNotas/TelaInclusaoNotas';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TelaInicial />} />
-      <Route path="/painel-aluno" element={<PainelAluno />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<TelaInicial />} />
+        <Route path="/painel-aluno" element={<PainelAluno />} />
+        <Route path="/inclusao-notas" element={<TelaInclusaoNotas />} /> {/* Verifique esta linha */}
+      </Routes>
+    </div>
   );
 }
 
