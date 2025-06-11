be3139c04c768f9d854c26108e1082351c2f2172
Igor Mateus Mordaski <igormordaski9@gmail.com>
2025-06-10 22:22:22 -0300

    Salvando algumas telas

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2e4fb023b2e9116c15c8de5e13e8dab6960a65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f23783c35f0816e28921539bbf4f838f2bc992c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
